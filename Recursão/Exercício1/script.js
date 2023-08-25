6d450f6945c67869da91fdfbe774162956ba34f5
Guilherme Henrique <112288334+guibr7@users.noreply.github.com>
2023-08-25 16:43:54 -0300

    Soma valores de uma lista com recursão

--- Recursão/Exercício1/script.js
+++ Recursão/Exercício1/script.js
@@ -1,13 +1,23 @@
 /*Escreva o código para a função soma, vista anteriormente*/
 
 /*Retornar o valor total com Loop*/
+/*
 function soma(lista){
    let total = 0
     for(let i = 0; i <= lista.length-1 ; i++ ){      
         total+=lista[i]
     }
     return console.log(total)
+}*/
+
+function soma(lista){
+    if(lista.length === 0){
+        return 0 
+    } else {
+        return lista[0]+soma(lista.slice(1))
+    }
 }
-let array = [7,7,2]
-soma(array)
+
+let lista = [7,7,2]
+soma(lista)
 
